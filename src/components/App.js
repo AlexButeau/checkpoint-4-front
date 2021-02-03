634d2a505bef970b15e0f0f7fd788de97a4ad77f
AlexButeau <alexbuteau1@gmail.com>
2021-02-03 18:22:21 +0100

    created first route

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,22 @@
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Login from './Login';
 
 function App() {
-  return <div className="App">Wild P3</div>;
+  return (
+    <Router>
+      <div className="app">
+        {/* <Header /> */}
+        <main>
+          <Switch>
+            <Route path="/">
+              <Login />
+            </Route>
+          </Switch>
+        </main>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
